b6c1ed302a4914339397b4e48f0cd3076a536b22
zjarlin <zjarlin@outlook.com>
2025-07-04 22:38:16 +0800

    change

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `kotlin-dsl`
+//    `kotlin-dsl-base`
+
 }
 
 repositories {
@@ -9,7 +11,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
-    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.32.0")
+    implementation(libs.gradlePlugin.android)
+    implementation(libs.gradlePlugin.dokka)
+    implementation(libs.gradlePlugin.jetbrainsCompose)
+    implementation(libs.gradlePlugin.composeCompiler)
+    implementation(libs.gradlePlugin.kotlin)
+    implementation(libs.gradlePlugin.mavenPublish)
+
+//    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+//    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.32.0")
 //    implementation("com.android.tools.build:gradle:8.3.0")
 }
--- buildSrc/src/main/kotlin/org/babyfish/jimmer/addzero-spring.gradle.kts
+++
@@ -1,6 +0,0 @@
-import org.babyfish.jimmer.P.myGroup
-import org.babyfish.jimmer.P.myVersion
-
-//group = "com.addzero.kmp"
-group = myGroup
-version =myVersion
