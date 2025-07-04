444a9c216a112a9c2a183ec16aef707c255319da
zjarlin <zjarlin@outlook.com>
2025-07-04 22:27:11 +0800

    change

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.32.0")
 //    implementation("com.android.tools.build:gradle:8.3.0")
 }
--- buildSrc/src/main/kotlin/org/babyfish/jimmer/publish-convention.gradle.kts
+++ buildSrc/src/main/kotlin/org/babyfish/jimmer/publish-convention.gradle.kts
@@ -12,37 +12,37 @@ group = groupVal
 version = versionVal
 
 // 临时注释发布相关配置，直到基本构建可工作
-// plugins {
-//    id("com.vanniktech.maven.publish")
-// }
+ plugins {
+    id("com.vanniktech.maven.publish")
+ }
 
-// mavenPublishing {
-//    publishToMavenCentral(automaticRelease = true)
-//    signAllPublications()
-//    coordinates(groupVal, p.name, versionVal)
-//    pom {
-//        name.set(projName)
-//        description.set(projName)
-//        inceptionYear.set("2025")
-//        url.set(giturl)
-//        licenses {
-//            license {
-//                name.set("The Apache License, Version 2.0")
-//                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//            }
-//        }
-//        developers {
-//            developer {
-//                id.set(authorName)
-//                name.set(authorName)
-//                url.set("https://gitee.com/$authorName")
-//            }
-//        }
-//        scm {
-//            url.set(giturl)
-//            connection.set("scm:git:git://${giturl.removePrefix("https://")}.git")
-//            developerConnection.set("scm:git:ssh://git@${giturl.removePrefix("https://")}.git")
-//        }
-//    }
-// }
+ mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+    coordinates(groupVal, p.name, versionVal)
+    pom {
+        name.set(projName)
+        description.set(projName)
+        inceptionYear.set("2025")
+        url.set(giturl)
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set(authorName)
+                name.set(authorName)
+                url.set("https://gitee.com/$authorName")
+            }
+        }
+        scm {
+            url.set(giturl)
+            connection.set("scm:git:git://${giturl.removePrefix("https://")}.git")
+            developerConnection.set("scm:git:ssh://git@${giturl.removePrefix("https://")}.git")
+        }
+    }
+ }
