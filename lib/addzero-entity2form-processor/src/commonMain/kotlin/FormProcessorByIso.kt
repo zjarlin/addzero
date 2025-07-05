a7717809c6952fa4369fa8146457dbdf69eb3c12
zjarlin <zjarlin@outlook.com>
2025-07-05 23:23:20 +0800

    change

--- lib/addzero-entity2form-processor/src/commonMain/kotlin/FormProcessorByIso.kt
+++ lib/addzero-entity2form-processor/src/commonMain/kotlin/FormProcessorByIso.kt
@@ -1,5 +1,4 @@
 
-import com.addzero.kmp.context.SettingContext
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
--- lib/addzero-ksp-support/src/commonMain/kotlin/com/addzero/kmp/context/SettingContext.kt
+++ lib/addzero-ksp-support/src/commonMain/kotlin/com/addzero/kmp/context/SettingContext.kt
@@ -1,34 +1,35 @@
 import com.addzero.kmp.context.Settings
+import com.addzero.kmp.util.BeanUtil.mapToBean
+import com.addzero.kmp.util.BeanUtil.toMap
 import java.util.concurrent.atomic.AtomicReference
 
 object SettingContext {
     private val settingsRef = AtomicReference<Settings?>()
 
     val settings: Settings
-        get() = settingsRef.get() ?: throw IllegalStateException("Settings not initialized")
+        get() = settingsRef.get() ?: Settings()
 
     fun initialize(op: Map<String, String>) {
-        settings.toMap()
-        val newSettings =settings.apply {
+        val toMap = settings.toMap()
+        val map = toMap + op
+        val mapToBean = mapToBean<Settings>(map)
+        settingsRef.compareAndSet(null, mapToBean)
 
-        }
-
-
-         Settings(
-            dbType = op["dbType"] ?: throw IllegalArgumentException("ksp option dbType required"),
-            idType = op["idType"] ?: throw IllegalArgumentException("ksp option idType required"),
-            id = op["id"] ?: throw IllegalArgumentException("ksp option id required"),
-            createBy = op["createBy"] ?: throw IllegalArgumentException("ksp option createBy required"),
-            updateBy = op["updateBy"] ?: throw IllegalArgumentException("ksp option updateBy required"),
-            createTime = op["createTime"] ?: throw IllegalArgumentException("ksp option createTime required"),
-            updateTime = op["updateTime"] ?: throw IllegalArgumentException("ksp option updateTime required"),
-            skipExistsFiles = op["skipExistsFiles"] ?: throw IllegalArgumentException("ksp option skipExistsFiles required"),
-            sharedSourceDir = op["sharedSourceDir"] ?: throw IllegalArgumentException("ksp option sharedSourceDir required"),
-            isomorphicPackageName = op["isomorphicPackageName"] ?: throw IllegalArgumentException("ksp option isomorphicPackageName required"),
-            isomorphicClassSuffix = op["isomorphicClassSuffix"] ?: throw IllegalArgumentException("ksp option isomorphicClassSuffix required"),
-            formPackageName = op["formPackageName"] ?: throw IllegalArgumentException("ksp option formPackageName required"),
-            apiClientPackageName = op["apiClientPackageName"] ?: throw IllegalArgumentException("ksp option apiClientPackageName required")
-        )
-        settingsRef.compareAndSet(null, newSettings)
+//         Settings(
+//            dbType = op["dbType"] ?: throw IllegalArgumentException("ksp option dbType required"),
+//            idType = op["idType"] ?: throw IllegalArgumentException("ksp option idType required"),
+//            id = op["id"] ?: throw IllegalArgumentException("ksp option id required"),
+//            createBy = op["createBy"] ?: throw IllegalArgumentException("ksp option createBy required"),
+//            updateBy = op["updateBy"] ?: throw IllegalArgumentException("ksp option updateBy required"),
+//            createTime = op["createTime"] ?: throw IllegalArgumentException("ksp option createTime required"),
+//            updateTime = op["updateTime"] ?: throw IllegalArgumentException("ksp option updateTime required"),
+//            skipExistsFiles = op["skipExistsFiles"] ?: throw IllegalArgumentException("ksp option skipExistsFiles required"),
+//            sharedSourceDir = op["sharedSourceDir"] ?: throw IllegalArgumentException("ksp option sharedSourceDir required"),
+//            isomorphicPackageName = op["isomorphicPackageName"] ?: throw IllegalArgumentException("ksp option isomorphicPackageName required"),
+//            isomorphicClassSuffix = op["isomorphicClassSuffix"] ?: throw IllegalArgumentException("ksp option isomorphicClassSuffix required"),
+//            formPackageName = op["formPackageName"] ?: throw IllegalArgumentException("ksp option formPackageName required"),
+//            apiClientPackageName = op["apiClientPackageName"] ?: throw IllegalArgumentException("ksp option apiClientPackageName required")
+//        )
+//        settingsRef.compareAndSet(null, newSettings)
     }
 }
