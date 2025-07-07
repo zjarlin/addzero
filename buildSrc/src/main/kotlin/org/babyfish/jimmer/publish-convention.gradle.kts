45d4943f928cc7773c835fc3ba93b4d9c0123ee9
zjarlin <zjarlin@outlook.com>
2025-07-07 19:59:27 +0800

    change

--- buildSrc/src/main/kotlin/org/babyfish/jimmer/publish-convention.gradle.kts
+++ buildSrc/src/main/kotlin/org/babyfish/jimmer/publish-convention.gradle.kts
@@ -1,42 +1,12 @@
 import org.babyfish.jimmer.Vars
 
 plugins {
-//       signing
     id("com.vanniktech.maven.publish")
 }
-//val keystorePropertiesFile = rootProject.file("local.properties")
-//
-//val keystoreProperties = Properties()
-//
-//keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-
-//signing {
-//    val mavenCentralUsername = findProperty("mavenCentralUsername")?.toString()
-//    val mavenCentralPassword = findProperty("mavenCentralPassword")?.toString()
-//    val keyId = findProperty("signing.keyId")?.toString()
-//    val password = findProperty("signing.password")?.toString()
-//    val keyFilePath = findProperty("signing.secretKeyRingFile")?.toString()
-//
-//    println("mavenCentralUsername: $mavenCentralUsername")
-//    println("mavenCentralPassword: $mavenCentralPassword")
-//    println("signing.keyId: $keyId")
-//    println("signing.password: $password")
-//    println("signing.secretKeyRingFile: $keyFilePath")
-//
-//    val secretKeyContent = file(keyFilePath!!).readText(Charsets.UTF_8)
-//    println("Secret Key Content: $secretKeyContent")
-//
-//    useInMemoryPgpKeys(keyId, secretKeyContent, password)
-//    sign(publishing.publications)
-//}
 
 mavenPublishing {
     publishToMavenCentral(automaticRelease = true)
     signAllPublications()
-}
-
-
-mavenPublishing {
 //    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     coordinates(project.group.toString(), project.name, project.version.toString())
 
