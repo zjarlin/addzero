8a400166738571092c17d5b6058000176aa21dd0
zjarlin <zjarlin@outlook.com>
2025-07-18 11:59:38 +0800

    change

--- composeApp/src/commonMain/kotlin/com/addzero/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/addzero/kmp/App.kt
@@ -2,10 +2,8 @@ package com.addzero.kmp
 
 import androidx.compose.runtime.Composable
 import com.addzero.kmp.component.toast.ToastListener
-import com.addzero.kmp.core.network.AddHttpClient
 import com.addzero.kmp.events.EventBusConsumer
 import com.addzero.kmp.events.emitEventBus
-import com.addzero.kmp.settings.SettingContext4Compose
 import com.addzero.kmp.ui.auth.LoginScreen
 import com.addzero.kmp.ui.infra.MainLayout
 import com.addzero.kmp.ui.infra.theme.AppThemes
