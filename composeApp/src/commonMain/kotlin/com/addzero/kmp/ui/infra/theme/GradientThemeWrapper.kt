95e69a45ea56954bcd868bb88ae0f7c125a7ad30
zjarlin <zjarlin@outlook.com>
2025-07-19 20:30:31 +0800

    change

--- composeApp/src/commonMain/kotlin/com/addzero/kmp/ui/infra/theme/GradientThemeWrapper.kt
+++ composeApp/src/commonMain/kotlin/com/addzero/kmp/ui/infra/theme/GradientThemeWrapper.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -18,6 +19,7 @@ fun SidebarGradientBackground(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
+
     val gradientConfig = AppThemes.getGradientConfig(themeType)
 
     if (gradientConfig != null && themeType.isGradient()) {
